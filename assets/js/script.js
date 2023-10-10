c4ba9ef1f3c19b1b5197d6e1c7779ca831d9c932
VladaAlek <vladimiraleksic.nis@gmail.com>
2023-10-10 12:57:47 +0000

    add updateScore function for playGuest and playHost

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,54 @@
 // algorithm 
 
 // initialize the score
-let score1 = =;
+let score1 = 0;
 let score2 = 0;
 
 // ad event listeners to Host and Guest buttons
 // attach the functions to the event listeners
-document.getElementById('playBtnHost').addEventListener('click', /*add function*/);
-document.getElementById('playBtnGuest').addEventListener('click', /*add function*/);
+document.getElementById('playBtnHost').addEventListener('click', playHost);
+document.getElementById('playBtnGuest').addEventListener('click', playGuest);
+document.getElementById('resetBtn').addEventListener('click', resetGame);
 
-// functions for the event listeners
+function playHost() {
+    updateScore(1);
+}
+
+function playGuest() {
+    updateScore(2);
+}
+
+function updateScore(player) {
+    // random score between 0 and 3
+    let score = Math.floor(Math.random() * 4);
+
+    // Show the respective image based on the score
+    if (score === 0) {
+        document.getElementById('miss').style.display = 'block';
+    } else if (score === 1) {
+        document.getElementById('onePoint').style.display = 'block';
+    } else if (score === 2) {
+        document.getElementById('twoPoints').style.display = 'block';
+    } else if (score === 3) {
+        document.getElementById('threePoints').style.display = 'block';
+    }
+    
+    //increase the score
+    if (player === 1) {
+        score1 += score;
+        // update the score
+        document.getElementById('score1').innerHTML = score1;
+    } else {
+        score2 += score;
+        //update the score
+        document.getElementById('score2').innerHTML = score2;
+    }
+}
+
+// functions for the event listeners for players buttons
 
 // ad event listeners to Reser button
 
-document.getElementById('resetBtn').addEventListener('click', /*add function*/);
 // find the way to declere the winner
 
 // create function to update score
@@ -22,8 +57,6 @@ document.getElementById('resetBtn').addEventListener('click', /*add function*/);
 
 // find the way to hide previous images
 
-// random score between 0 and 3
-let score = Math.floor(Math.random() * 4);
 
 // Show the image based on the score *
 
