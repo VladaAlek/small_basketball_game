c4def2fde41528f1c21bef1fc3ffb71de99d910b
VladaAlek <vladimiraleksic.nis@gmail.com>
2023-10-10 09:20:16 +0000

    add script algorithm, initial score values, add event listeners to all three buttons and random number generator

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// algorithm 
+
+// initialize the score
+let score1 = =;
+let score2 = 0;
+
+// ad event listeners to Host and Guest buttons
+// attach the functions to the event listeners
+document.getElementById('playBtnHost').addEventListener('click', /*add function*/);
+document.getElementById('playBtnGuest').addEventListener('click', /*add function*/);
+
+// functions for the event listeners
+
+// ad event listeners to Reser button
+
+document.getElementById('resetBtn').addEventListener('click', /*add function*/);
+// find the way to declere the winner
+
+// create function to update score
+
+// create function to reset game
+
+// find the way to hide previous images
+
+// random score between 0 and 3
+let score = Math.floor(Math.random() * 4);
+
+// Show the image based on the score *
+
+// Check for winner
+
+
